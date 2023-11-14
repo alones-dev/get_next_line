b59e84b1e4a03ea1d369ecf3e531e3e0a23bac44
Kylian Daumont <kdaumont@bess-f3r5s7.clusters.42paris.fr>
2023-11-14 16:08:53 +0100

    start test

--- get_next_line.c
+++ get_next_line.c
@@ -6,10 +6,34 @@
 /*   By: kdaumont <kdaumont@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/14 13:46:50 by kdaumont          #+#    #+#             */
-/*   Updated: 2023/11/14 13:52:09 by kdaumont         ###   ########.fr       */
+/*   Updated: 2023/11/14 15:51:48 by kdaumont         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "get_next_line.h"
+
+void	ft_putchar(int c)
+{
+	write(1, &c, 1);
+}
+
 char	*get_next_line(int fd)
 {
+	char	*buffer[5000];
+	int		i;
+
+	i = 0;
+	read(fd, buffer, 5000);
+	while (buffer[i])
+	{
+		ft_putchar(*buffer);
+		i++;
+	}
+}
+
+void	main(void)
+{
+	int test = open("test.txt", O_RDONLY);
+	get_next_line(test);
+	close(test);
 }
--- get_next_line.h
+++ get_next_line.h
@@ -6,12 +6,15 @@
 /*   By: kdaumont <kdaumont@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/14 13:46:54 by kdaumont          #+#    #+#             */
-/*   Updated: 2023/11/14 13:52:11 by kdaumont         ###   ########.fr       */
+/*   Updated: 2023/11/14 15:12:11 by kdaumont         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
+
+# include <fcntl.h>
+# include <unistd.h>
 
 char	*get_next_line(int fd);
 
